e37e364a43efe49bfc1524991b2fc8ba4096f0d4
y.a <yankin.aa@mail.ru>
2018-02-15 18:49:54 +0300

    task 4 end

--- src/index.js
+++ src/index.js
@@ -4,3 +4,34 @@ export default() => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName} !`);
 };
+
+
+
+export const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName} !`);
+
+ const iter = (counter) => {
+   const random = () => Math.floor(Math.random() * (100 - 0)) + 0;
+   const randomNum = random();
+   const evenNum = () => randomNum % 2 === 0 ? 'yes' : 'no';
+   const even = evenNum();
+  console.log(`Question: ${randomNum}`);
+  if (counter > 3) {
+    return console.log(`Congratulations, ${userName}!`);
+}
+
+const yourAnswer = readlineSync.question('Your answer: ');
+
+
+if (even === yourAnswer) {
+  console.log('Correct!');
+  return iter(counter + 1);
+}
+ return console.log(`${yourAnswer} is wrong answer ;(. Correct answer was ${even}.\nLet's try again, ${userName}!`);
+};
+return iter(1);
+};
