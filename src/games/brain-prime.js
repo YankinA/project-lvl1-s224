edcc9fef30dcf0b777ab1f84eb63d3055fbd640b
y.a <yankin.aa@mail.ru>
2018-02-18 10:54:02 +0300

    task 9

---
+++ src/games/brain-prime.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { cons } from 'hexlet-pairs';
+import { random } from '..';
+import { starGame } from '..';
+
+export const taskPrime = 'Is this number prime?';
+
+const isPrime = (num) => {
+  if (num === 1) {
+    return false;
+  }
+  if (num === 2) {
+    return true;
+  }
+
+ for(let i = 2;i < num; i++) {
+    if(num % i === 0) {
+      return false;
+    } 
+  }
+return true;
+};
+
+ 
+export const questionAnswer = () => {
+  const questionPrime = random(100, 1);
+  const correctAnswerPrime = isPrime(questionPrime) ? 'yes' : 'no';
+  return cons(questionPrime, correctAnswerPrime);
+};
+
+export default () => starGame(taskPrime, questionAnswer);
+
+ 
